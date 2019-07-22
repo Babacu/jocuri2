791343e32039aacea6a115075018c3686d0281ee
Sebi Ghiara <sebi.gg98@gmail.com>
2019-07-23 02:11:36 +0300

    I improved the games

--- calculator.py
+++ calculator.py
@@ -1,56 +1,141 @@
-# This function adds two numbers
-def add(x, y):
-    return x+y
-# This function subtracts two numbers
-def subtract(x, y):
-    return x-y
-# This function multiply two numbers
-def multiply(x, y):
-    return x*y
-# This function divide two numbers
-def divide(x, y):
-    return x/y
+print("X si 0")
+print(" ")
+
+# the spaces bellow represent the position where the players will put x or 0 
+board = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
+
+# This is the game board
+def board():
+    print(board[0], ' |', board[1], '|', board[2])
+    print('___|___|___')
+    print(board[3], ' |', board[4], '|', board[5])
+    print('___|___|___')
+    print(board[6], ' |', board[7], '|', board[8])
+    print('   |   |   ')
+    print(" ")
+# These are the two lists which contains the position of x and 0
+positionsOfX = ["x,1", "x,2", "x,3", "x,4", "x,5", "x,6", "x,7", "x,8", "x,9"]
+positionsOf0 = ["0,1", "0,2", "0,3", "0,4", "0,5", "0,6", "0,7", "0,8", "0,9"]
+
+tabla()
 
 loop = 1
-
-alegere = 0
-
-print("Buna ziua!")
-# Welcoming the user
-greeting = input()
-print("Cum va numiti? ")
-#Your name
-name = input()
-print(f"Buna {name}, bine ai venit pe calculator.py")
-# write 2 numbers
-num1 = int(input("Introdu primul numar: "))
-num2 = int(input("Introdu al doilea numar: "))
 while loop == 1:
-    print(" ")
-    print("1) Adunare") #Is the sum of the numbers
-    print("2) Scadere") #The subtraction of the numbers
-    print("3) Inmultire") #The multiplication of the numbers
-    print("4) Impartire") #The division of the numbers
-    print("5) Nu sunt interesat") #You're not interested in what this app can do
-    print(" ")
-    # Choose which operation you want the computer to do
-    print("Alegeti optiunea dorita: ")
-    operation = int(input())
-    if operation == 1:
-        print("Suma celor doua numere este: ", add(num1, num2))
-    elif operation == 2:
-        print("Diferenta numerelor este: ", subtract(num1, num2))
-    elif operation == 3:
-        print("Produsul numerelor este: ", multiply(num1, num2))
-    elif operation == 4:
-        print(" Numarul ", num1, "impartit la ", num2, "este egal cu: ", divide(num1, num2))
-    elif operation == 5:
-        # leave the app
-        print("Sunteti sigur ca dorit sa parasiti calculator.py? da sau nu?")
-        leave = input()
-        if leave == "da":
-            loop = 0
+    loop2 = 1
+    loop3 = 1
+    while loop2 == 1:
+        # Is the first player's turn
+        print("Randul lui x")
+        #The first player decides in which position he will place x
+        positionOfX = input()
+        if positionOfX == positionsOfX[0]:
+            board[0] = "x"
+        elif positionOfX == positionsOfX[1]:
+            board[1] = "x"
+        elif positionOfX == positionsOfX[2]:
+            board[2] = "x"
+        elif positionOfX == positionsOfX[3]:
+            board[3] = "x"
+        elif positionOfX == positionsOfX[4]:
+            board[4] = "x"
+        elif positionOfX == positionsOfX[5]:
+            board[5] = "x"
+        elif positionOfX == positionsOfX[6]:
+            board[6] = "x"
+        elif positionOfX == positionsOfX[7]:
+            board[7] = "x"
+        elif positionOfX == positionsOfX[8]:
+            board[8] = "x"
         else:
-            print("ok")
+            # type one position from 1 to 9, not other ones
+            print("Nu inteleg ce doriti sa scrieti!")
+            print("Va rugam, incercati din nou!")
+            # please, insert the position again
             continue
-print("Va multumim ca ati folosit calculator.py")
+        board()
+        if "x" == board[0] and board[0] == board[1] and board[1] == board[2]:
+            loop3 = 0
+            loop = 0
+            print("x a castigat")
+        elif "x" == board[2] and board[2] == board[5] and board[5] == board[8]:
+            loop3 = 0
+            loop = 0
+            print("x a castigat")
+        elif "x" == board[6] and board[6] == board[7] and board[7] == board[8]:
+            loop3 = 0
+            loop = 0
+            print("x a castigat")
+        elif "x" == board[0] and board[0] == board[3] and board[3] == board[6]:
+            loop3 = 0
+            loop = 0
+            print("x a castigat")
+        elif "x" == board[0] and board[0] == board[4] and board[4] == board[8]:
+            loop3 = 0
+            loop = 0
+            print("x a castigat")
+        elif "x" == board[2] and board[2] == board[4] and board[4] == board[6]:
+            loop3 = 0
+            loop = 0
+            print("x a castigat")
+        elif "x" == board[1] and board[1] == board[4] and board[4] == board[7]:
+            loop3 = 0
+            loop = 0
+            print("x a castigat")
+        elif "x" == board[3] and board[3] == board[4] and board[4] == board[5]:
+            loop3 = 0
+            loop = 0
+            print("x a castigat")
+        loop2 = 0
+    while loop3 == 1:
+        print("Randul lui 0")
+        #Now it's second player's turn who decides where to place 0
+        positionOfZero = input()
+        if positionOfZero == positionsOf0[0]:
+            board[0] = "0"
+        elif positionOfZero == positionsOf0[1]:
+            board[1] = "0"
+        elif positionOfZero == positionsOf0[2]:
+            board[2] = "0"
+        elif positionOfZero == positionsOf0[3]:
+            board[3] = "0"
+        elif positionOfZero == positionsOf0[4]:
+            board[4] = "0"
+        elif positionOfZero == positionsOf0[5]:
+            board[5] = "0"
+        elif positionOfZero == positionsOf0[6]:
+            board[6] = "0"
+        elif positionOfZero == positionsOf0[7]:
+            board[7] = "0"
+        elif positionOfZero == positionsOf0[8]:
+             board[8] = "0"
+        else:
+            print("Nu inteleg ce vreti sa scrieti!")
+            print("Va rugam, incercati din nou!")
+            continue
+        board()
+        if "0" == board[0] and board[0] == board[1] and board[1] == board[2]:
+            loop = 0
+            print("0 a castigat")
+        elif "0" == board[2] and board[2] == board[5] and board[5] == board[8]:
+            loop = 0
+            print("0 a castigat")
+        elif "0" == board[6] and board[6] == board[7] and board[7] == board[8]:
+            loop = 0
+            print("0 a castigat")
+        elif "0" == board[0] and board[0] == board[3] and board[3] == board[6]:
+            loop = 0
+            print("0 a castigat")
+        elif "0" == board[0] and board[0] == board[4] and board[4] == board[8]:
+            loop = 0
+            print("0 a castigat")
+        elif "0" == board[2] and board[2] == board[4] and board[4] == board[6]:
+            loop = 0
+            print("0 a castigat")
+        elif "0" == board[1] and board[1] == board[4] and board[4] == board[7]:
+            loop = 0
+            print("0 a castigat")
+        elif "0" == board[3] and board[3] == board[4] and board[4] == board[5]:
+            loop = 0
+            print("0 a castigat")
+        loop3 = 0
+print("Va multumim!")
